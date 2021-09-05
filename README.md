3799fb3fd9bc19c45556d5ea3f41a5d1462b7123
mariliamessias <marilia.oliveira@outlook.com>
2021-09-05 20:57:51 -0300

    add readme

---
+++ README.md
@@ -0,0 +1,47 @@
+## 🧪 Tecnologias
+
+Esse projeto foi desenvolvido com as seguintes tecnologias:
+
+- [React](https://reactjs.org)
+
+## 🧪 Telas
+
+Home:
+
+<h1 align="center">
+    <img alt="DtMoney" src="./src/assets/images/home.png" />
+</h1>
+Página do Carrinho:
+<br>
+<br>
+<h1 align="center">
+    <img alt="DtMoney" src="./src/assets/images/cart.png" />
+</h1>
+
+## 🚀 Como executar
+
+Clone o projeto e acesse a pasta do mesmo.
+
+```bash
+$ git clone https://github.com/mariliamessias/ignite-cart
+$ cd ignite-movies
+```
+
+Para iniciá-lo, siga os passos abaixo:
+```bash
+# Instalar as dependências
+$ yarn
+
+# Iniciar o projeto
+$ yarn start
+```
+O app estará disponível no seu browser pelo endereço http://localhost:3000.
+
+## 💻 Projeto
+
+Projeto do curso Ignite da Rocketseat que tem como objetivo fixar os conhecimentos introdutórios do segundo módulo, aplicando conceitos de context, hooks, props, components e tests.
+
+
+## 📝 License
+
+Esse projeto está sob a licença MIT. Veja o arquivo [LICENSE](LICENSE.md) para mais detalhes.
--- src/hooks/useCart.tsx
+++ src/hooks/useCart.tsx
@@ -1,3 +1,5 @@
+import { useEffect } from 'react';
+import { useRef } from 'react';
 import { createContext, ReactNode, useContext, useState } from 'react';
 import { toast } from 'react-toastify';
 import { api } from '../services/api';
@@ -23,28 +25,77 @@ const CartContext = createContext<CartContextData>({} as CartContextData);
 
 export function CartProvider({ children }: CartProviderProps): JSX.Element {
   const [cart, setCart] = useState<Product[]>(() => {
-    // const storagedCart = Buscar dados do localStorage
+    const storagedCart = localStorage.getItem('@RocketShoes:cart');
 
-    // if (storagedCart) {
-    //   return JSON.parse(storagedCart);
-    // }
+    if (storagedCart) {
+      return JSON.parse(storagedCart);
+    }
 
     return [];
   });
 
+  const prevCart = useRef<Product[]>();
+
+  useEffect(() => {
+    prevCart.current = cart;
+  })
+
+  const cartPreviousValue = prevCart.current ?? cart;
+
+  useEffect(( ) => {
+    if(cartPreviousValue !== cart){
+      localStorage.setItem('@RocketShoes:cart', JSON.stringify(cart));
+    }
+  }, [cart, cartPreviousValue])
+
   const addProduct = async (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart];
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      const stock = await api.get(`/stock/${productId}`);
+
+      const stockAmount = stock.data.amount;
+      const currentAmount = productExists ? productExists.amount : 0;
+      const amount = currentAmount + 1;
+
+      if(amount > stockAmount){
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      if(productExists){
+          productExists.amount = amount;
+      }else{
+        const product = await api.get(`/products/${productId}`);
+        const newProduct = {
+          ...product.data, 
+          amount: 1
+        };
+
+        updatedCart.push(newProduct);
+      }
+
+      setCart(updatedCart);
+
     } catch {
-      // TODO
+      toast.error('Erro na adição do produto');
     }
   };
 
   const removeProduct = (productId: number) => {
     try {
-      // TODO
+      const updatedCart = [...cart];
+      const productIndex = updatedCart.findIndex(product => product.id === productId);
+      if(productIndex >= 0){
+        updatedCart.splice(productIndex, 1);
+        setCart(updatedCart);
+
+      }else{
+        throw Error();
+      }
     } catch {
-      // TODO
+      toast.error('Erro na remoção do produto');
     }
   };
 
@@ -53,9 +104,30 @@ export function CartProvider({ children }: CartProviderProps): JSX.Element {
     amount,
   }: UpdateProductAmount) => {
     try {
-      // TODO
+      if(amount <= 0){
+        return;
+      }
+
+      const stock = await api.get(`/stock/${productId}`);
+      const stockAmount = stock.data.amount;
+      if(amount > stockAmount){
+        toast.error('Quantidade solicitada fora de estoque');
+        return;
+      }
+
+      const updatedCart = [...cart];
+      const productExists = updatedCart.find(product => product.id === productId);
+
+      if(productExists){
+        productExists.amount = amount;
+        setCart(updatedCart);
+
+      }else{
+        throw Error();
+      }
+
     } catch {
-      // TODO
+      toast.error('Erro na alteração de quantidade do produto');
     }
   };
 
